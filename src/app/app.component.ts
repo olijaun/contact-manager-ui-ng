6c9486a1e57682c68cc50129b61baf9489980b94
Jaun Oliver ( U112266 ) <oliver.jaun@mobi.ch>
2018-05-18 17:11:55 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public title = 'asdfasdf';
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  ngOnInit() {
+    this.http.get('http://google.com')
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
